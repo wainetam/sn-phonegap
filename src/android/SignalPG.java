f534c614928d5801176ee604affd83ddbbaccec1
Waine Tam <wainetam@gmail.com>
2015-07-23 12:08:40 -0400

    testing meta data tag

--- src/android/SignalPG.java
+++ src/android/SignalPG.java
@@ -75,7 +75,9 @@ public class SignalPG extends CordovaPlugin implements SignalClient, SignalUICli
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         Signal.get().initialize(cordova.getActivity().getApplicationContext(), this, "ZjAwNmM3ZTgtOTkzNS00ZjMxLTk4ZmUtNzRhNDNiNDQzZWE1");
-        SignalUI.get().initialize(cordova.getActivity().getApplicationContext(), this, cordova.getActivity().getApplicationContext().getResources().getClass());
+        // SignalUI.get().initialize(cordova.getActivity().getApplicationContext(), this, cordova.getActivity().getApplicationContext().getResources().getClass());
+        SignalUI.get().initialize(cordova.getActivity().getApplicationContext(), this, com.signal360.plugin.mainpackage.R.class);
+        
         // SignalUI.get().initialize(cordova.getActivity().getApplicationContext(), this, R.class);
         Signal.get().start();
     }
