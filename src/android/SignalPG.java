404fbb20836c8210ae25bb9dfaea9f52f6509936
Waine Tam <wainetam@gmail.com>
2015-07-23 12:30:43 -0400

    testing meta data tag

--- src/android/SignalPG.java
+++ src/android/SignalPG.java
@@ -22,6 +22,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.ViewGroup;
