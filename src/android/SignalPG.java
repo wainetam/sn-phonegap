1daddedb087c0acae932fd7de3708a9021deeca1
Waine Tam <wainetam@gmail.com>
2015-07-23 12:50:09 -0400

    testing meta data tag

--- src/android/SignalPG.java
+++ src/android/SignalPG.java
@@ -80,12 +80,12 @@ public class SignalPG extends CordovaPlugin implements SignalClient, SignalUICli
         Signal.get().initialize(cordova.getActivity().getApplicationContext(), this, "ZjAwNmM3ZTgtOTkzNS00ZjMxLTk4ZmUtNzRhNDNiNDQzZWE1");
         // SignalUI.get().initialize(cordova.getActivity().getApplicationContext(), this, cordova.getActivity().getApplicationContext().getResources().getClass());
 
-        Context context = cordova.getActivity().getApplicationContext();
-        ApplicationInfo ai = context.getPackageManager().getApplicationInfo(this.getPackageName(), PackageManager.GET_META_DATA);
-        Bundle aBundle=ai.metaData;
-        String aValue=aBundle.getString("com.signal360.plugin.mainpackage");
+        // Context context = cordova.getActivity().getApplicationContext();
+        // ApplicationInfo ai = context.getPackageManager().getApplicationInfo(this.getPackageName(), PackageManager.GET_META_DATA);
+        // Bundle aBundle=ai.metaData;
+        // String aValue=aBundle.getString("com.signal360.plugin.mainpackage");
 
-        SignalUI.get().initialize(cordova.getActivity().getApplicationContext(), this, Class.forName(aValue + ".R"));
+        SignalUI.get().initialize(cordova.getActivity().getApplicationContext(), this, Class.forName("com.example.hello.R"));
         
         // SignalUI.get().initialize(cordova.getActivity().getApplicationContext(), this, R.class);
         Signal.get().start();
