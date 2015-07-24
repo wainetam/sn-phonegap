0bce7fcba403ca9607ad72344c7c4cee82935b73
Waine Tam <wainetam@gmail.com>
2015-07-24 02:39:43 -0400

    edited isbluetoothenabled

--- src/android/SignalPG.java
+++ src/android/SignalPG.java
@@ -59,6 +59,7 @@ import java.util.Iterator;
 public class SignalPG extends CordovaPlugin implements SignalClient, SignalUIClient {
 
     private static final String TAG = "PhoneGap/Signal360";
+    private CallbackContext callbackContext;
     // Signal methods
     private static final String INITIALIZE="initialize";
     private static final String START="start";
@@ -88,7 +89,7 @@ public class SignalPG extends CordovaPlugin implements SignalClient, SignalUICli
 
     @Override
     public boolean execute(String action, JSONArray arguments, CallbackContext callbackContext) throws JSONException {
-
+        this.callbackContext = callbackContext;
         try {
             if (INITIALIZE.equals(action)) {
                 String appID = arguments.getString(0);
@@ -118,7 +119,7 @@ public class SignalPG extends CordovaPlugin implements SignalClient, SignalUICli
                 Boolean enabled = Signal.get().isBluetoothEnabled();
                 Log.d("SIGNALPG", "isBluetoothEnabled " + enabled);
                 cordova.getActivity().runOnUiThread(new Runnable() {
-                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, enabled));
+                    this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, enabled));
                     // callbackContext.success((boolean)enabled);
                 });
 
