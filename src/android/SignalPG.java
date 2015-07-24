2103cf5cf8d0962015fee26937b45329bd5aa558
Waine Tam <wainetam@gmail.com>
2015-07-24 02:30:25 -0400

    edited isbluetoothenabled

--- src/android/SignalPG.java
+++ src/android/SignalPG.java
@@ -118,8 +118,10 @@ public class SignalPG extends CordovaPlugin implements SignalClient, SignalUICli
                 Boolean enabled = Signal.get().isBluetoothEnabled();
                 Log.d("SIGNALPG", "isBluetoothEnabled " + enabled);
                 cordova.getActivity().runOnUiThread(new Runnable() {
-                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, (boolean) enabled));
+                    // callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, enabled));
+                    callbackContext.success(enabled);
                 });
+                
                 return true;
             } else if (IS_ADVERTISING_IDENTIFIER_ENABLED.equals(action)) {
                 Boolean enabled = Signal.get().isAdvertisingIdentifierEnabled();
